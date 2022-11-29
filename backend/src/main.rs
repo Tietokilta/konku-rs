841c7f4cef087be5cd7f6985b3442f04330c2841
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2022-11-29 21:57:12 +0200

    Further request parsing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,12 @@
-use axum::{routing::get, Router};
+use axum::{
+    extract::{DefaultBodyLimit, Multipart},
+    routing::post,
+    Router,
+};
+use hyper::StatusCode;
+use mime::Mime;
 use std::net::SocketAddr;
-use tower_http::trace::TraceLayer;
+use tower_http::{limit::RequestBodyLimitLayer, trace::TraceLayer};
 use tracing_subscriber::{
     filter::{EnvFilter, LevelFilter},
     layer::SubscriberExt,
@@ -23,8 +29,11 @@ async fn main() {
         .init();
 
     let app = Router::new()
-        .route("/", get(handler))
+        .route("/", post(handler))
         // Create debug-level tracing events for HTTP requests
+        .layer(DefaultBodyLimit::disable())
+        // Limit request body to 250MB
+        .layer(RequestBodyLimitLayer::new(250 * 1024 * 1024))
         .layer(TraceLayer::new_for_http());
 
     let address = SocketAddr::from(([0, 0, 0, 0], 3000));
@@ -36,6 +45,35 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> &'static str {
-    "Hello, World!"
+async fn handler(mut multipart: Multipart) -> Result<String, (StatusCode, String)> {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?
+    {
+        let field_name = field
+            .name()
+            .map(|f| f.to_string())
+            .ok_or((StatusCode::BAD_REQUEST, "Malformed field name".to_string()))?;
+
+        let content_type = field
+            .content_type()
+            .map(|f| f.parse::<Mime>().unwrap_or(mime::APPLICATION_OCTET_STREAM))
+            .ok_or((
+                StatusCode::BAD_REQUEST,
+                "Malformed content type".to_string(),
+            ))?;
+
+        let data = field
+            .bytes()
+            .await
+            .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+
+        match field_name.as_str() {
+            "data" => Ok("Success".to_string()),
+            "attachments" => Ok("Success".to_string()),
+            _ => Err((StatusCode::BAD_REQUEST, "Unknown field name".to_string())),
+        }?;
+    }
+    Ok("Success".to_string())
 }
