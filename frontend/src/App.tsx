23503e332e165ec9b3ad15bfd1fcd958d6aaa956
Eemeli <eemelioksanen@protonmail.com>
2023-03-11 21:02:21 +0200

    move success notification to top center

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -60,7 +60,7 @@ const App = () => {
   return (
     <div className="relative pb-2">
       <ToastContainer
-        position="bottom-left"
+        position="top-center"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
